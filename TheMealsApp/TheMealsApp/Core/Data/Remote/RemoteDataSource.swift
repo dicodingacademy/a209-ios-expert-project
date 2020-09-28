43df9bbfb8868dcac5545687ef05ceb51f78e60e
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2020-09-28 17:12:20 +0700

    Change URLSession to Alamofire

--- TheMealsApp/TheMealsApp/Core/Data/Remote/RemoteDataSource.swift
+++ TheMealsApp/TheMealsApp/Core/Data/Remote/RemoteDataSource.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Alamofire
 
 protocol RemoteDataSourceProtocol: class {
 
@@ -29,20 +30,14 @@ extension RemoteDataSource: RemoteDataSourceProtocol {
   ) {
     guard let url = URL(string: Endpoints.Gets.categories.url) else { return }
 
-    let task = URLSession.shared.dataTask(with: url) { maybeData, maybeResponse, maybeError in
-      if maybeError != nil {
-        result(.failure(.addressUnreachable(url)))
-      } else if let data = maybeData, let response = maybeResponse as? HTTPURLResponse, response.statusCode == 200 {
-        let decoder = JSONDecoder()
-        do {
-          let categories = try decoder.decode(CategoriesResponse.self, from: data).categories
-          result(.success(categories))
-        } catch {
-          result(.failure(.invalidResponse))
+    AF.request(url)
+      .validate()
+      .responseDecodable(of: CategoriesResponse.self) { response in
+        switch response.result {
+        case .success(let value): result(.success(value.categories))
+        case .failure: result(.failure(.invalidResponse))
         }
-      }
     }
-    task.resume()
 
   }
 
